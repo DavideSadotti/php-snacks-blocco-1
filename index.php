2ba787174e98c8184af55bd84565901736da2d34
Davide <d.sadotti@gmail.com>
2021-05-04 12:50:24 +0200

    Snack 1

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+    $matches = [
+        "abc1" => [
+            "casa" => "Olimpia Milano",
+            "casaPunti" => 55,
+            "ospite" => "Cantù",
+            "ospitePunti" => 60
+        ],
+        "abc2" => [
+            "casa" => "Dinamo Sassari",
+            "casaPunti" => 89,
+            "ospite" => "Reggiana",
+            "ospitePunti" => 82
+        ],
+        "abc3" => [
+            "casa" => "Cremona",
+            "casaPunti" => 89,
+            "ospite" => " Brescia",
+            "ospitePunti" => 94
+        ],
+        "abc4" => [
+            "casa" => "Trento",
+            "casaPunti" => 77,
+            "ospite" => "Brindisi",
+            "ospitePunti" => 99
+        ],
+        "abc5" => [
+            "casa" => "Virtus Bologna",
+            "casaPunti" => 97,
+            "ospite" => "Universo Treviso",
+            "ospitePunti" => 68
+        ]
+    ];
+
+    foreach ($matches as $k => $match) {
+        echo "Partita: " . $k . "<br>" . $match["casa"] . " - " . $match["ospite"] . " | " . $match["casaPunti"] . " - " . $match["ospitePunti"] . "<br><br>";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    
+</body>
+</html>
